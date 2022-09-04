3c56a8bd526a84ed20005a35854d38221922c45a
Tungstend <hanjinlong197@gmail.com>
2022-09-04 16:45:14 +0800

    Update loadme.c

--- Boat/src/main/jni/loadme/loadme.c
+++ Boat/src/main/jni/loadme/loadme.c
@@ -34,12 +34,17 @@ JNIEXPORT void JNICALL Java_cosine_boat_LoadMe_setupExitTrap(JNIEnv *env, jclass
     xhook_refresh(1);
 }
 
+void (*__loader_dlopen)(const char* __filename, int __flag, const void* caller_addr);
 void (*old_dlopen)(const char* __filename, int __flag);
 void new_dlopen(const char* __filename, int __flag) {
-    return old_dlopen(__filename, __flag);
+    void* caller_addr = __builtin_return_address(0);
+    return __loader_dlopen(__filename, __flag, caller_addr);
 }
 
 JNIEXPORT void JNICALL Java_cosine_boat_LoadMe_hookDlopen(JNIEnv *env, jclass clazz) {
+    void* handle;
+    handle = dlopen("libdl.so", RTLD_LAZY);
+    __loader_dlopen = (void (*)(const char*, int, const void*))dlsym(handle, "__loader_dlopen");
     xhook_register(".*\\.so$", "dlopen", new_dlopen, (void **) &old_dlopen);
     xhook_refresh(1);
 }
