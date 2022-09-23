8f5887dc34678336bc2388be7d76116e90977519
LegacyGamerHD <wesleyvanneck1012@gmail.com>
2022-09-23 19:44:31 +0200

    Revert "remove old verification"
    This reverts commit 81ce6149b39ab4c7c74740f2ede9e81246dbe654.

--- HMCLPE/src/main/java/com/tungsten/hmclpe/utils/DigestUtils.java
+++ HMCLPE/src/main/java/com/tungsten/hmclpe/utils/DigestUtils.java
@@ -67,4 +67,48 @@ public final class DigestUtils {
         return digest;
     }
 
+    public static String encryptToMD5(String str) {
+        // 加密后的16进制字符串
+        String hexStr = "";
+        try {
+            // 此 MessageDigest 类为应用程序提供信息摘要算法的功能
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            // 转换为MD5码
+            byte[] digest = md5.digest(str.getBytes("utf-8"));
+            hexStr = bytesToHex(digest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return hexStr;
+    }
+    public static String bytesToHex(byte[] bytes)
+    {
+        StringBuffer sb = new StringBuffer();
+        if (bytes != null && bytes.length > 0)
+        {
+            for (int i = 0; i < bytes.length; i++) {
+                String hex = byteToHex(bytes[i]);
+                sb.append(hex);
+            }
+        }
+        return sb.toString();
+    }
+    public static String byteToHex(byte b)
+    {
+        String hexString = Integer.toHexString(b & 0xFF);
+        //由于十六进制是由0~9、A~F来表示1~16，所以如果Byte转换成Hex后如果是<16,就会是一个字符（比如A=10），通常是使用两个字符来表示16进制位的,
+        //假如一个字符的话，遇到字符串11，这到底是1个字节，还是1和1两个字节，容易混淆，如果是补0，那么1和1补充后就是0101，11就表示纯粹的11
+        if (hexString.length() < 2)
+        {
+            hexString = new StringBuilder(String.valueOf(0)).append(hexString).toString();
+        }
+        return hexString.toUpperCase();
+    }
+    public static String getDeviceCode(Context context) {
+        return DigestUtils.encryptToMD5(DeviceIdentifier.getOAID(context)
+                + DeviceIdentifier.getAndroidID(context)
+                + DeviceIdentifier.getWidevineID()
+                + DeviceIdentifier.getPseudoID());
+    }
+
 }
